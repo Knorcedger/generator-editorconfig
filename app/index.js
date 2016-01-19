4328161b1f7a5468c591beaa12469fc7d8e2abab
Achilleas Tsoumitas <knorcedger@gmail.com>
2016-01-19 12:20:10 +0200

    Changed to configuring phase (from writing)

--- app/index.js
+++ app/index.js
@@ -12,17 +12,16 @@ module.exports = generators.Base.extend({
 			message: 'Prefer tabs (t) or spaces (s) for indentation?',
 			default: 't'
 		}, function(answers) {
-			this.log(answers.indentation);
 			if (answers.indentation === 't') {
 				indentation = 'tab';
 			} else {
 				indentation = 'space';
 			}
 			done();
-		}.bind(this));
+		});
 	},
 	// write the editorconfig and update with the selected indentation character
-	writing: function() {
+	configuring: function() {
 		this.fs.copyTpl(
 			this.templatePath('editorconfig'),
 			this.destinationPath('.editorconfig'),
